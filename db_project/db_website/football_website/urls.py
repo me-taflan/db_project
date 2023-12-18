d673c787271c0e7a0bd598832266b9a0c28f9690
Yasin Uğurlu <yugurlu72@gmail.com>
2023-12-18 19:30:50 +0300

    all

--- db_project/db_website/football_website/urls.py
+++ db_project/db_website/football_website/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('match/<int:match_id>/', views.match_page, name='match_page'),
     path('league/<int:league_id>/', views.league_page, name='league_page'),
     path('team/<int:team_id>/', views.team_page, name='team_page'),
+    path('Players/',views.player_page,name='player_page'),
+    path('Players/<int:player_id>/',views.player_info_page,name='player_info')
 ]
